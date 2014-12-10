202a7737bb0a9197306f161fdabaae2e38d2b899
Kristina Ming <kming@hmc.edu>
2014-12-10 15:55:42 -0800

    Added integral control... potentially...

--- Kristina/satellite.m
+++ Kristina/satellite.m
@@ -16,27 +16,28 @@ thetardot = 0;
 mup = 10;
 mud = 10;
 
-disturbance = 10;
+I = 20;
+disturbance = 10000;
 
 P = J*wn^2;
 D = J*2*zeta*wn;
 
 % run the simulation
-tspan = [0:T:N*T];
+tspan = [0:T:1];
 [t,x,y] = sim('satelliteSim', tspan);
 plot(t,y(:,1),t,y(:,2),t,y(:,3),t,y(:,5),t,y(:,6))
 title('PD Control System (\mu_P = 10, \mu_D = 10)')
 xlabel('time (s)')
 legend('\theta', 'P', 'D','\theta dot', 'u = J\theta double dot')
 
-
-x1 = y(:,7);
-x2 = y(:,8);
-u = y(:,6);
-n = 1:N;
-I = 0;
-
-% performance metrics
-for n = 1:N
-    I = I + (T*((x1(n))^2+(x2(n))^2+((u(n))^2)/100))
-end
+% 
+% x1 = y(:,7);
+% x2 = y(:,8);
+% u = y(:,6);
+% n = 1:N;
+% I = 0;
+% 
+% % performance metrics
+% for n = 1:N
+%     I = I + (T*((x1(n))^2+(x2(n))^2+((u(n))^2)/100))
+% end
